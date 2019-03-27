67e3ed12217156d99f3636c56827c0ad4a29ec7e
Tommaso <Tommaso@DESKTOP-7Q04DPS.lan>
2019-03-27 21:24:51 +0100

    Non ho fatto niente parte 43frwe

--- Pro2/src/pro2/Pro2.java
+++ Pro2/src/pro2/Pro2.java
@@ -17,6 +17,10 @@ public class Pro2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("prova git 2");
+        System.out.println("prova git 2");
+        System.out.println("prova git 2");
+        System.out.println("prova git 2");
+        
     }
     
 }
